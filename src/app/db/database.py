ef5e233d4e740f5615a2dfd11e361fd20e74a867
Annnn <huuan04112002@gmail.com>
2022-08-26 10:04:07 +0700

    change database into postgresql

--- src/app/db/database.py
+++ src/app/db/database.py
@@ -1,12 +1,14 @@
 import os
+from dotenv import load_dotenv
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # path to save db
+# load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./db/sql_app.db"
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("DB_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
