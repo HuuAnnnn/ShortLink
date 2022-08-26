8c1e6f24079a7dd91ead408cfc364501b960c2bf
Annnn <huuan04112002@gmail.com>
2022-08-26 14:36:54 +0700

    add .env to run in the local

--- src/app/db/database.py
+++ src/app/db/database.py
@@ -1,11 +1,12 @@
 import os
+from dotenv import load_dotenv
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # path to save db
-# load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
+load_dotenv(os.path.join(os.path.dirname(__file__), "env/.env"))
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./db/sql_app.db"
 SQLALCHEMY_DATABASE_URL = os.getenv("DB_URL")
 
